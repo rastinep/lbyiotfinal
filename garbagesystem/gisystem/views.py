fed33f001a94f9a3555f40d664ae3afcae52e9b6
Rastine Pinlac <rastine_pinlac@dlsu.edu.ph>
2019-04-09 00:34:52 +0800

    Add files via upload

--- garbagesystem/gisystem/views.py
+++ garbagesystem/gisystem/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import serial
+import time
+from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -24,4 +28,15 @@ def overview(request):
 
 
 def hall(request):
-    return render(request, 'gisystem/hall.html', {})
+    serport = serial.Serial('/dev/cu.usbserial', '9600')
+    time.sleep(1)
+    serport.setDTR(False)
+    capacity = serport.readline()
+    capacity = capacity.decode('utf-8').rstrip()
+    return render(request, 'gisystem/hall.html', {'capacity': capacity})
+
+
+def ajax_data(request):
+    #values = MyNoiseLevelModel.objects.all().values('value')
+    json_data = json.dumps(data)
+    return Response(json_data, mimetypes='application/json')
